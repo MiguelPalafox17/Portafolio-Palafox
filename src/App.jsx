e36c48ebcaf29e687a2022191eccabc48ce8de18
MiguelPalafox17 <ma.palafox23@info.uas.edu.mx>
2025-06-21 12:38:54 -0700

    Subir proyecto creado

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Home_Presentacion from './Pages/Home-Presentacion/Home'
+
+function App() {
+
+  return (
+    <>
+    <Home_Presentacion />
+    </>
+  )
+}
+
+export default App;
---
+++ src/Pages/Home-Presentacion/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.css';
+
+function Home_Presentacion() {
+    return (
+        <div className="home-container">
+        <h1>Welcome to Our Website</h1>
+        <p>This is the home page where you can find the latest updates and information.</p>
+        <button className="home-button">Learn More</button>
+        </div>
+    );
+}
+
+export default Home_Presentacion;
